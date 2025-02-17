1a6e9ee77dcd9f269ec758f9fc5a50c34cb4cb54
iPhantomiKX <cmpsun98@gmail.com>
2025-02-17 16:11:01 +0800

    Added Debug Draw Functionality in C++ Code + MACROS

--- Source/ProjectBossRush5/Private/Items/Item.cpp
+++ Source/ProjectBossRush5/Private/Items/Item.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Items/Item.h"
+#include "ProjectBossRush5/DebugMacros.h"
 
 AItem::AItem()
 {
@@ -13,15 +14,30 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+	// UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+	//
+	// if (GEngine)
+	// 	GEngine->AddOnScreenDebugMessage(1, 60.0f, FColor::Red, FString("Item OnScreen Message!"));
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
 
-	if (GEngine)
-		GEngine->AddOnScreenDebugMessage(1, 60.0f, FColor::Red, FString("Item OnScreen Message!"));
+	DRAW_SPHERE(Location);
+	//DRAW_LINE(Location, Location + Forward * 100.0f);
+	//DRAW_POINT(Location + Forward * 100.0f);
+	DRAW_VECTOR(Location, Location + Forward * 100.0f);
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+	// UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"), DeltaTime);
+	// if (GEngine)
+	// {
+	// 	FString Name = GetName();
+	// 	FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+	// 	GEngine->AddOnScreenDebugMessage(1, 60.0f, FColor::Red, Message);
+	// 	UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
+	// }
 }
 
